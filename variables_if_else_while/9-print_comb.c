020d6caecc28b7d593605afd28fd95f9b4d2288f
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-21 11:21:50 +0000

    ajoute9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This function prints all possible combinations
+ * of single-digit numbers,
+ * separated by ", " in ascending order.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	while (x <= 9)
+	{
+		putchar(x + '0');
+	}
+		if (x != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+		x++
+	return (0);
+}
