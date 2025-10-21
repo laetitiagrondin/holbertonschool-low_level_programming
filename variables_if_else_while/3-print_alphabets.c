eed013520f9ab5d3e987a1bbcbb86a4c81c5d9e0
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-21 08:48:36 +0000

    ajoute3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -12,14 +12,14 @@ int main(void)
 {
 	char n;
 
-	for (n = 'a'; n <= 'z', n++);
+	for (n = 'a'; n <= 'z'; n++)
 	{
 		putchar(n);
 	}
-	for (n = 'A'; n <= 'Z', n++);
+	for (n = 'A'; n <= 'Z'; n++)
 	{
 		putchar(n);
-		putchar('\n')
+	putchar('\n');
 	}
 	return (0);
 }
