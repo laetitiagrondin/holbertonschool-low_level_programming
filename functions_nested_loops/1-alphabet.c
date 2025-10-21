352fe50934a0fdc7ec817d21336b5bb1cb5221e4
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-21 12:53:47 +0000

    AJOUTER1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: This function prints the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
