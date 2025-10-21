52f31790fbccb98e18b018b7eaa1b477c31d1c10
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-21 08:21:03 +0000

    ajoute2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: This function prints the alphabet
+ * in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
