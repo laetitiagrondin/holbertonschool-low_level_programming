16d8f7e76e5b90701845d7da1ecf1843b2fd2cce
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-22 11:51:28 +0000

    AJOUTER8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,6 +13,7 @@ void jack_bauer(void)
 	int hour, minute;
 
 	for (hour = 0; hour < 24; hour++)
+	{
 		for (minute = 0; minute < 60; minute++)
 		{
 			_putchar(hour / 10 + '0');
@@ -22,4 +23,5 @@ void jack_bauer(void)
 			_putchar(minute % 10 + '0');
 			_putchar('\n');
 		}
+	}
 }
