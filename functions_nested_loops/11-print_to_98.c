1794ccc5d4ce2364803f1bbcb1a9fca937302d0c
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-23 09:21:17 +0000

    AJOUTER11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,19 +13,19 @@ void print_to_98(int n)
 {
 	int i;
 
-        if (n < 98)
-        {
-                for (i = n; i < 98; i++)
-                {
-                        printf("%d, ", i);
-                }
-        }
-        else if (n > 98)
-        {
-                for (i = n; i > 98; i--)
-                {
-                        printf("%d, ", i);
-                }
-        }
-        printf("98\n");
+	if (n < 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+	}
+	printf("98\n");
 }
