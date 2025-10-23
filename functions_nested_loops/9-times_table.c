60e5096f511f05b0d74d6419f61fb114e0bae1e8
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-23 07:51:51 +0000

    AJOUTER9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,18 +12,30 @@ void times_table(void)
 	int i, j, pr;
 
 	for (i = 0; i < 10; i++)
+	{
 		for (j = 0; j < 10; j++)
+		{
 			pr = i * j;
 			if (j == 0)
+			{
 				_putchar(pr + '0');
+			}
 			else
+			{
 				_putchar(',');
 				_putchar(' ');
-			if (pr < 10)
-				_putchar(' ');
-				_putchar(pr + '0');
-			else
-				_putchar(pr / 10 + '0');
-				_putchar(pr % 10 + '0');
+				if (pr < 10)
+				{
+					_putchar(' ');
+					_putchar(pr + '0');
+				}
+				else
+				{
+					_putchar(pr / 10 + '0');
+					_putchar(pr % 10 + '0');
+				}
+			}
+		}
 		_putchar('\n');
+	}
 }
