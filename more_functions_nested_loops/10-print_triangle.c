203f247664d5cfd50da438565c9a2a8c50134083
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-27 16:02:16 +0000

    AJOUTE10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -14,11 +14,11 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = size -1; j > 0; j--)
+			for (j = size - 1; j > 0; j--)
 			{
 				_putchar(' ');
 			}
-			for (j = 0; j < size; j++)
+			for (j = 0; j <= size; j++)
 			{
 				_putchar('#');
 			}
