622799557c3fbad7a80de6656a22b11860924ad7
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-27 12:17:27 +0000

    AJOUTE6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - Draws a straight line in the terminal
+ *
+ * Return: Nothing
+ */
+void print_line(int_n)
+{
+	if (n == 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
