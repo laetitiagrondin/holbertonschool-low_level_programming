53fd84e5c09fd10b24210a7a37de01537d833a32
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-27 09:24:56 +0000

    AJOUTER3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - Prints the number, from 0 to 9, followed by a new line
+ *
+ * Return: Nothing
+ */
+void print_numbers(void);
+{
+	int i;
+
+	for (i = 0; for i <=9; i++)
+		_putchar(i);
+	_putchar('\n');
+}
