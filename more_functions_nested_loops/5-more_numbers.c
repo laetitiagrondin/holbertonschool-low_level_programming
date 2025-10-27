2189e0e580b2706645f1b4ff4287bd5dc72f4632
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-27 10:06:15 +0000

    AJOUTE5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,6 @@ void more_numbers(void)
 	int i;
 
 	for (i = 0; i <= 14; i++)
-	{
-		if (i < 10)
-		{
-			_putchar(i);
-		}
-		else
-		{
-			_putchar(i + '0');
-		}
-	}
+		_putchar(i + '0');
+		_putchar('\n');
 }
