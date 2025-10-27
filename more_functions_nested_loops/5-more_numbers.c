e3aa63055f5b7324fb999b608ed029387fcb92be
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-27 10:46:35 +0000

    AJOUTE5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,14 +10,17 @@ void more_numbers(void)
 {
 	int i, j;
 	
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (i > 9)
+			if (i <= 9)
 			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
+				_putchar(i + '0');
+			}
+			else
+			{
+				_putchar((i / 10) + (i % 10) + '0');
 			}
 		}
 		_putchar('\n');
