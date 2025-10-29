2a99facac5243eb9efd7bab78d11e90566c96846
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-29 16:18:40 +0000

    AJOUT2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string
+ * @
+ */
+int _strlen(char *s);
+{
+	int i;
+
+	int i = 0;
+	while (i != '\0')
+	{
+		_putchar(*s[i]);
+	}
+	i++;
+}
