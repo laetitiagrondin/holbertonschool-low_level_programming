f0ffd6373600d435106f17cb8811d6a1a832d56c
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-29 10:56:10 +0100

    AJOUTER8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,7 +6,7 @@
  * Description: This function prints every minute of the day of Jack Bauer,
  * starting from 00:00 to 23:59.
  *
- * Return: Always 0 (Success)
+ * Return: Void
  */
 void jack_bauer(void)
 {
