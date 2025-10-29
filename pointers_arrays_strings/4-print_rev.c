458e9b31e26cc598f3052d5eb58bc24ee72e43d9
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-29 17:02:52 +0000

    AJOUT4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string, in reverse, followed by a new line
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++
+	}
+	while (s[i] > 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n')
+}
