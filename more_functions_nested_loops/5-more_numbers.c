f608054c953df98bb9c8729383767d01ec47b335
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-29 10:15:49 +0100

    AJOUTE5
    Updated the return type in the documentation to reflect that the function does not return a value.

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
  * more_numbers - Prints 10 times the numbers, from 0 to 14,
  * followed by a new line
  *
- * Return: 10 times of the numbers since 0 up to 14
+ * Return: Void
  */
 void more_numbers(void)
 {
