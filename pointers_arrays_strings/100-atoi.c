91bf3ca3537aa26b4149a611ce93ae44b1309978
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-30 20:46:22 +0000

    AJOUT10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _atoi - Converts a string to an integer
+ * @s : Pointer to the string to convert
+ *
+ * Return: the integer obtained from the string,
+ * or 0 if no number is found
+ */
+int _atoi(char *s)
+{
+        int i;
+        int len = 0;
+
+        for (i = 0; i < len; i++)
+        {
+                while (i <= 0 && i >= 9)
+                {
+                        print(i);
+                        i++;
+                }
+        }
+}
