8f613761b8f8df8ce4c5317cbb4b5e07fd7e6a17
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-30 17:44:55 +0000

    AJOUT8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_array - Prints n elements of an array of integers,
+ * followed by a new line
+ * @n: number of elements
+ */
+void print_array(int *a, int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+		{
+			printf("%d", i)
+		}
+		else
+		{
+			printf(", %d", i);
+		}
+	}
+}
