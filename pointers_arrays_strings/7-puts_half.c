c09a0f2bb3865fb8831158f8cd3cad49405138d5
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-30 13:49:33 +0000

    AJOUT7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - Prints half of a string, followed by a new line
+ * @str:
+ *
+ */
+void puts_half(char *str)
+{
+	int n;
+	int len = strlen(str);
+
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len + 1) / 2;
+	}
+
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n++
+	}
+	_putchar('\n');
