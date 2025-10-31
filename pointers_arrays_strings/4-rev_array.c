2b8c67030eae05083c0121169f5ee13072749380
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-31 14:17:06 +0000

    AJ4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reverse_array - Reverses the content of an array of integers
+ * @n: number of elements of the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	char temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
