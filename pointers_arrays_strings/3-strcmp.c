b24de108eeec652534b179f2b5cd1c4783d772c0
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-31 13:52:33 +0000

    AJ3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,13 +10,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0');
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] < s2[i]);
+		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
-			i++;
 		}
+		i++;
 	}
 	return (s1[i] - s2[i]);
 }
