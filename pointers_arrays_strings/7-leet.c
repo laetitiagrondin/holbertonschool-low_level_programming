61679869fdfda69a74a62a5fd1c6d8fa4c814a9e
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-31 22:32:34 +0000

    AJ7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * leet - Encodes a string into 1337
+ * @s: the string to be encoded
+ *
+ * Return: The encoded string
+ */
+char *leet(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+		{
+			s[i] = '4';
+		}
+		else if (s[i] == 'e' || s[i] == 'E')
+		{
+			s[i] = '3';
+		}
+		else if (s[i] == 'o' || s[i] == 'O')
+		{
+			s[i] = '0';
+		}
+		else if (s[i] == 't' || s[T] == 'T')
+		{
+			s[i] = '7';
+		}
+		else if (s[i] == 'l' || s[i] == 'L')
+		{
+			s[i] = '1';
+		}
+		i++;
+	}
+	return s;
+}
