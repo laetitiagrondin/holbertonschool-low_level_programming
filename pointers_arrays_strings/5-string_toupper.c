ec828eebe9916b44206f6028bfcbd0c5af027ebc
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-31 21:34:08 +0000

    AJ5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,10 @@
+#include <ctype.h>
 #include "main.h"
 /**
  * string_toupper - Changes all lowercase letters of a string to uppercase
  * @char: a pointer to the string to be modified
  *
- * Return: The modified string with all lowercase letters
- * converted to uppercase
+ * Return: The modified string with all uppercase letters
  */
 char *string_toupper(char *str)
 {
