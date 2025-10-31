85bc53228c7e36ac92d1b80d8c2fab79603e1ee2
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-31 10:38:52 +0000

    AJ1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - Concatenates two strings
+ * @dest: pointer to the destination string where the content is appended
+ * @src: pointer to the source string to be appended to dest
+ * @n: number of bytes
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
