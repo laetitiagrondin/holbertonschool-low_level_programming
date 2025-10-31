a42e992c5112c884b767a52bf865cea7d69717e6
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-31 13:59:36 +0000

    AJ3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,9 @@
  * @s1: 1st string
  * @s2: 2nd string
  *
- * Return:
+ * Return: 0 if the strings are equal,
+ * a negative value if s1 < s2,
+ * a positive value if s1 > s2
  */
 int _strcmp(char *s1, char *s2)
 {
