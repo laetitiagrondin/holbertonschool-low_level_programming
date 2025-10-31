62115e2e166b38d91effa799695757b19fea8446
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-10-31 09:52:55 +0000

    AJ0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - Concatenates two strings
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	_putchar('\0');
+	return (dest);
+}
