4f64ca65ce31a73b53f8331489bd44a4e371dfc6
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-03 13:53:41 +0000

    AJOUTE1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - Copies memory area
+ * @dest: pointer to the destination area
+ * @src: pointer to the source area
+ * @n: number of bytes to copy
+ *
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (s);
+}
