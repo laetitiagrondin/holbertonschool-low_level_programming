5ef48eb1c5ba7504288e88a577ecc1a221051750
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-04 13:30:14 +0000

    AJOUTE0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -9,11 +9,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-	
-	for (i = 0; i < n; i++)
+	unsigned int i = 0;
+
+	while (i < n)
 	{
 		s[i] = b;
+		i++;
 	}
 	return (s);
 }
