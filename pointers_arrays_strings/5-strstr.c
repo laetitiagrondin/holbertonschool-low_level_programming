4322c7c76ff42621767e7b7b52a8165d14b83614
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-04 15:34:46 +0000

    AJOUTE5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,7 +12,9 @@ char *_strstr(char *haystack, char *needle)
 	int i, j, m;
 	
 	if (needle[0] == '\0')
+	{
 		return (haystack);
+	}
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		m = i;
@@ -23,7 +25,9 @@ char *_strstr(char *haystack, char *needle)
 			j++;
 		}
 		if (needle[j] == '\0')
+		{
 			return (haystack + i);
+		}
 	}
 	return (NULL);
 }
