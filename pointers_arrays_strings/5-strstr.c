e530a16ffabd0875a15ed03fe43154ecc706da55
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-04 16:56:20 +0000

    AJOUTE5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,18 +12,18 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 	
-	if (*needle == 0)
+	if (*needle == '\0')
 	{
 		return (haystack);
 	}
-	for (i = 0; haystack[i] != 0; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		j = 0;
-		while (needle[j] != 0 && haystack[i + j] == needle[j])
+		while (needle[j] != '\0' && haystack[i + j] == needle[j])
 		{
 			j++;
 		}
-		if (needle[j] == *0)
+		if (needle[j] == '\0')
 		{
 			return (haystack + i);
 		}
