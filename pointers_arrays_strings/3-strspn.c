487718470fb46d8803066304ce253987ca7f92fe
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-04 13:33:54 +0000

    AJOUTE3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -14,7 +14,7 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0' &&
-				s[i] != accept[j]; j++);
+				s[i] != accept[j]; j++)
 		{
 			if (accept[j] == '\0')
 			{
