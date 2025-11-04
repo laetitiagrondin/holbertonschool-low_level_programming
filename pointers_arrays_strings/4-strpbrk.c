a89949dce5a7d169f3b917adc5f5caae92a0e83d
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-04 14:16:15 +0000

    AJOUTE4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -9,18 +9,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	unsigned int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accet[j])
+			if (s[i] == accept[j])
 			{
 				return (s + i);
 			}
 		}
-
 	}
-	return NULL;
+	return (NULL);
 }
