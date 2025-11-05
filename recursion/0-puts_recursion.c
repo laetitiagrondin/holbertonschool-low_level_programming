20a520c7c33d5c7974cec710aefba39c5b5ace2d
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-05 11:29:06 +0000

    AJOUTER0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -9,11 +9,8 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		printf("\n");
+		_putchar('\n');
 	}
-	else
-	{
-		printf("%c", *s);
-		_puts_recursion(s + 1);
-	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
