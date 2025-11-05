0961e69dfa636c27c5575498f223c09479aca427
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-05 12:18:13 +0000

    AJOUTE2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: The length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+	{
+		return (i);
+	}
+	_strlen_recursion(s + 1);
+	i++;
+}
