77af52dc5af52e1f36491ed5101d34592add28f3
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-05 12:54:33 +0000

    ajoute3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -11,9 +11,9 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-	else if (n = 0)
+	else if (n == 0)
 	{
-		return (0);
+		return (1);
 	}
-	factorial(n * (n - 1));
+	return (n * factorial(n - 1));
 }
