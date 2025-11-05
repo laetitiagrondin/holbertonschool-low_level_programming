e1cad349d02ac250a12bf3c38e7e815888e0a0aa
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-05 12:24:23 +0000

    AJOUTER2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -7,12 +7,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
 	if (*s == '\0')
 	{
-		return (i);
+		return (0);
 	}
-	_strlen_recursion(s + 1);
-	i++;
+	return (1 + _strlen_recursion(s + 1));
 }
