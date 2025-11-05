721989b427cc2ea6e85b9f343632386ed25b1746
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-05 10:15:08 +0000

    AJOUTER1

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string, followed by a new line
+ * @s: pointer to the string
+ *
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	if (s != '\0')
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
