ff76d454452e72f1908c604cb9d9d4fa3d8e85d9
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-06 13:03:42 +0000

    AJOUTER6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int _is_prime(int n, int i)
 	{
 		return (1);
 	}
-	if ((n != 2 && n % 2 == 0) || n % i == 0)
+	if (n % i == 0)
 	{
 		return(0);
 	}
@@ -30,6 +30,10 @@ int is_prime_number(int n)
 	if (n < 2)
 	{
 		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
 	}
 	return (_is_prime(n, 3));
 }
