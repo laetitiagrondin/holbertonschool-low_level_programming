16089d866591c47eb6025aa8da00beb2644209ee
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-07 10:19:40 +0000

    AJOUTER1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the number of arguments passed into the program
+ * @argc: number of arguments
+ * array of argument strings
+ *
+ * Return: Always 0 (Success)
+ */
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		argv[i];
+	}
+	printf("%d\n", argc - 1);
+	return (0);
+}
