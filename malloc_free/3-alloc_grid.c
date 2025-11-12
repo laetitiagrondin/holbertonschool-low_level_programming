c94f750c14129423cf474a127261a332e7ee5eee
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-12 10:30:11 +0000

    AJOUTER3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,28 +10,29 @@
  */
 int **alloc_grid(int width, int height)
 {
-	char *grid;
+	int **grid;
 	int i, j;
 
-	if (width == 0 || height == 0)
+	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 	grid = malloc(sizeof(int *) * height);
-	if (grid = NULL)
+	if (grid == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
-		if (grid == NULL)
+		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
 			{
 				free(grid[j]);
 			}
 			free(grid);
+			return (NULL);
 		}
 		for (j = 0; j < width; j++)
 		{
