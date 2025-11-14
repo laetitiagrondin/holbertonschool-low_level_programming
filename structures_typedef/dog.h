5c87288516e6c7d2295a9050e61d35698b721d23
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-14 08:58:38 +0000

    AJOUTER0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure representating a dog
+ * @name: name of the dog (char *)
+ * @age: age of the dog (float)
+ * @owner: name of the dog's owner (char *)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char owner;
+}
+
+#endif
