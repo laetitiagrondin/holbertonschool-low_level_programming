b3727d071cff0839fd24b582346666f12b006913
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-14 13:51:50 +0000

    AJOUTER5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib>
+#inclure "main.h"
+/**
+ * free_dog - Frees memory allocated for a dog structure
+ * @: pointer to the dog_t structure to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
