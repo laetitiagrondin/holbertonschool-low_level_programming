e9b1c2431d0bc1fc6cf681e0d1c5b5fe850ea47e
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-14 10:17:37 +0000

    AJOUTER2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,9 +15,17 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name);
 	}
+	else
+	{
+		printf("Name: (nil)\n");
+	}
 	printf("Age: %.6f\n", d->age);
 	if (d->owner != NULL)
 	{
 		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: (nil)\n");
 	}
 }
