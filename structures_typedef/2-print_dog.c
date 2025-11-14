10aec37d697ccc2efe5fa33c0e071db415a5a5d6
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-14 10:08:30 +0000

    AJOUTER2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_dog - prints the information of a struct dog
+ * @d: pointer to the struct dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name != NULL)
+	{
+		printf("Name: %s\n", d->name);
+	}
+	printf("Age: %.1f\n", d->age);
+	if (d->owner != NULL)
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+}
