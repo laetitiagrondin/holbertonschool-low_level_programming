bcfd7996b4bbdc84eb17ca69a511861fc3629fda
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-18 10:06:11 +0000

    AJOUTER0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_name - prints a name using a function pointer
+ * @name: the name to be printed
+ * @f: pointer to a function that takes a char pointer and returns nothing
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
