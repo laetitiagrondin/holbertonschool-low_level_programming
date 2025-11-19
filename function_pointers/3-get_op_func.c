a8d7e83229fec4d1f741fc431448b8805753413c
laetitiagrondin <laetitiagrondin>
2025-11-19 16:28:51 +0000

    Updates

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include "3-calc.h"
 /**
- * get_op_func - A
- * @s: a
+ * get_op_func - Returns a pointer
+ * to the function corresponding to an operator
+ * @s: the operator as a string
  *
- * Return: A
+ * Return: A pointer to the function associated with the operator,
+ * or NULL if the opertor is invalid
  */
 int (*get_op_func(char *s))(int, int)
 {
