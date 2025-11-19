49b02d3dc361dad18dc0cc2160f5d93fb6808b22
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-19 10:12:14 +0000

    MAIN

---
+++ function_pointers/main.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/** main - Performs simple operations
+ * @argc: number of elements
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, or exits with error codes
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, res;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[2] == '/' || *argv[2] == %) && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	res = op_func(num1, num2);
+	return (0);
+}
