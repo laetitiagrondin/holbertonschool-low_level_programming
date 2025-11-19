e075dd6fbc153fded66112144615ed77598f5dbb
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-19 10:32:40 +0000

    Added "stddef.h" and changement from "main.h" to "function_pointers.h"

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <stddef.h>
+#include "function_pointers.h"
 /**
  * array_iterator - Function that executes a function given
  * as a parameter on each element of an array
