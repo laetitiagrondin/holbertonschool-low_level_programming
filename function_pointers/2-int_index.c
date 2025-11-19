5173ef5b4f619977c6af2f9fce33ba84a96ae53f
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-19 11:31:54 +0000

    "main.h" to "function_pointers.h" and added <stddef.h>

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <stddef.h>
+#include "function_pointers.h"
 /**
  * int_index -  Function that searches for an integer
  * @array: the array to search in
@@ -19,7 +20,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	}
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) != 0);
+		if (cmp(array[i]) != 0)
 		{
 			return (i);
 		}
