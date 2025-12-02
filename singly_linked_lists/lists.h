b6a4923143785ec328ac7a9db685b2b807b98191
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-02 09:21:56 +0000

    Ajout de struct lists

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,24 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/**
+ * struct lists - A node in a singly linked list
+ * @str: pointer to char stored in the node
+ * @head: a pointer to the first node in the list
+ * @new: a pointer to another node
+ * @n: the number of nodes
+ */
+
+struct lists
+{
+	char *str;
+	struct lists *head;
+	struct lists *new;
+	size_t n;
+};
+
+typedef struct lists lists_t
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 
