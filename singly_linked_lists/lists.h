6ef3d517f7645fafa150a2aff021bcc878421bea
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-02 13:38:51 +0000

    - "head" en "h" : changements de noms de variables - "new" en "next" : changements de noms de variables + modification dans la description - Ajout de ";" apres size_t print_list(const list_t *h) dans struct lists

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -4,20 +4,20 @@
 /**
  * struct lists - A node in a singly linked list
  * @str: pointer to char stored in the node
- * @head: a pointer to the first node in the list
- * @new: a pointer to another node
+ * @h: a pointer to the first node in the list
+ * @next: a pointer to the next node
  * @n: the number of nodes
  */
 
 struct lists
 {
 	char *str;
-	struct lists *head;
-	struct lists *new;
+	struct lists *h;
+	struct lists *next;
 	size_t n;
 };
 
-typedef struct lists lists_t
+typedef struct lists lists_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
