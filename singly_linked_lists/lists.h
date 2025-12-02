c8cd3e17a20bda4594814e23d4d536f642d434fe
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-02 14:56:40 +0000

    Changement de ma structure de donnees en structure dans l'exemple

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,22 +2,19 @@
 #define LISTS_H
 
 /**
- * struct lists - A node in a singly linked list
- * @str: pointer to char stored in the node
- * @h: a pointer to the first node in the list
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: a pointer to the next node
- * @n: the number of nodes
+ *
+ * Description: singly linked list node structure
  */
-
-struct lists
+typedef struct list_s
 {
 	char *str;
-	struct lists *h;
-	struct lists *next;
-	size_t n;
-};
-
-typedef struct lists lists_t;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
