607fdbd1ab016b09decec69f63226761e8f5243c
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-03 14:37:44 +0000

    -> Ajout de bibliotheque : - stddef.h : NULL -> Implementation de la fonction get_dnodeint

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include "lists.h"
+/**
+ * get_dnodeint - Returns the nth node of a dlistint_t linked list
+ * @head: pointer to the head of a list
+ * @index: index of the node, starting from 0
+ *
+ * Return: A pointer to the node, or NULL if it does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		if (i == index)
+		{
+			return (head);
+		}
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
