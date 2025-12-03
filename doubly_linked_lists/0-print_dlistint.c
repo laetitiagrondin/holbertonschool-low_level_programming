441cfabb728fe8b7ddb2cf7b6100499b8c9bf6f5
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-03 10:23:43 +0000

    --- Ajout de bibliotheques : - stdio.h : printf - stddef.h : NULL et size_t - lists.h : structure et prototypes --- Implementation de fonction 0-print_dlistint.c

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list
+ * @h: pointer to the head of the doubly linked list
+ *
+ * Return: Number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h);
+{
+	size_t nb = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		nb++;
+		h = h->next;
+	}
+	return (nb);
+}
