3e5568bf5856cd6a74613e3f446b15e6839335bb
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-03 09:24:47 +0000

    --- Ajout de bibliotheques : - stdlib.h : malloc and free - string.h : strdup - stddef.h : NULL - lists.h : ajout de list_t *add_node_end(list_t **head, const char *str); --- Implementation de la fonction "add_node_end"

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node_end - Adds a new node at the end of a list_t list
+ * @head: double pointer to the beginning of the list
+ * @str: string to duplicate and store in the new node
+ *
+ * Return: Address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *last;
+	unsigned int len = 0;
+	int i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL);
+	{
+		free(new_node);
+	}
+	new_node->len = len;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	last = *head;
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+	last->next = new_node;
+	return (new_node);
+}
