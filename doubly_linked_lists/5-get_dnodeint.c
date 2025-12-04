954f0de4877620789bdf19d06e364e45d6d69dc9
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-04 14:16:34 +0000

    -> Bout de code ajoute avant le while

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,6 +11,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	while (head != NULL)
 	{
 		if (i == index)
