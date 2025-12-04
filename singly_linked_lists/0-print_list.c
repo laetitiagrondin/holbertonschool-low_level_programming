b6611fda4bb3497c70aca5d03a2b118895535846
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-04 12:17:56 +0000

    -> Modification dans printf : - %lu en %zu

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -20,7 +20,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%lu] %s\n", strlen(h->str), h->str);
+			printf("[%zu] %s\n", strlen(h->str), h->str);
 		}
 		h = h->next;
 		n++;
