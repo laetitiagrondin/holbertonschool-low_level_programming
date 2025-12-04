eb49fda1c609f3fde3eeaa34454bacfeffa6b5bf
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-04 14:21:55 +0000

    -> Quelques corrections de "mode" en "node"

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -4,13 +4,13 @@
 /**
  * add_dnodeint - Adds a new node at the beginning of a dlistint_t list
  * @head: pointer to the head of the list
- * @n: integer to store in the new mode
+ * @n: integer to store in the new node
  *
  * Return: Address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_mode;
+	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
