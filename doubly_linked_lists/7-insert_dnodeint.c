ed9b53014d459e53af13175eb295a7c583b15741
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-04 10:45:15 +0000

    -> Ajout de bibliotheques : - stddef.h : NULL - stdlib.h : malloc - lists.h : dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n); -> Implementation de la fonction insert_dnodeint

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,42 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_dnodeint - Inserts a new node at a given position
+ * @h: double pointer to the head of the list
+ * @idx: index of the list where the new node should be added (starts at 0)
+ * @n: integer to store in the new node
+ *
+ * Return: Address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *current;
+	int i;
+
+	if (h == NULL)
+	{
+		return (NULL);
+	}
+	current = *h;
+	for (i = 0; i < idx; i++)
+	{
+		current = current->next;
+	}
+	if (current == NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = current->next;
+	new_node->prev = current;
+	current->next->prev = new_node;
+	current->next = new_node;
+	return (new_node);
+}
