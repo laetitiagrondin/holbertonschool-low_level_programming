5f11ec7af749056bd8ae0e66bda34f6171646a0f
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-04 14:13:21 +0000

    -> Ajout du #endif a la fin

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -25,3 +26,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+#endif
