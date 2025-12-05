73b40246f2636eb974cd9416d624d99789c93e88
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-05 10:46:23 +0000

    -> Dans if (bytes_read == -1) : - Ajout du "="

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -33,7 +33,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	bytes_read = read(fd, buffer, letters);
-	if (bytes_read = -1)
+	if (bytes_read == -1)
 	{
 		free(buffer);
 		close(fd);
