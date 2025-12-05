3dc97f66d3911028d4bf244bec60c76cd8ec40b7
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-05 10:08:02 +0000

    Suppressions de {} pour moins de lignes

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -6,7 +6,6 @@
  * @h: double pointer to the head of the list
  * @idx: index of the list where the new node should be added (starts at 0)
  * @n: integer to store in the new node
- *
  * Return: Address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
@@ -16,31 +15,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 
 	if (h == NULL)
-	{
 		return (NULL);
-	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 	new_node->n = n;
 	if (idx == 0 || *h == NULL)
 	{
 		new_node->prev = NULL;
 		new_node->next = *h;
 		if (*h != NULL)
-		{
 			(*h)->prev = new_node;
-		}
 		*h = new_node;
 		return (new_node);
 	}
 	current = *h;
 	for (i = 0; (i < idx - 1) && (current->next != NULL); i++)
-	{
 		current = current->next;
-	}
 	if (i < idx - 1)
 	{
 		free(new_node);
@@ -49,9 +40,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next = current->next;
 	new_node->prev = current;
 	if (current->next != NULL)
-	{
 		current->next->prev = new_node;
-	}
 	current->next = new_node;
 	return (new_node);
 }
