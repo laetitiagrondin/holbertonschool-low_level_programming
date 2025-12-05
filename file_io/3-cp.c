e89f3013fb85d537e06358657c5929d39066aad6
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-05 17:20:23 +0000

    -> 664 en 0664

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -24,7 +24,7 @@ int copy_file(const char *file_from, const char *file_to)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 664);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
