2533d068108c228e52ad7fb658da97430a318874
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-05 17:27:58 +0000

    - Separation de dprintf et exit pour lignes 23 et 34 ^ |__ ajout de quelques {}

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -20,7 +20,10 @@ int copy_file(const char *file_from, const char *file_to)
 
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
@@ -31,7 +34,10 @@ int copy_file(const char *file_from, const char *file_to)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
 	}
 	if (bytes_read == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
 	if (close(fd_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 	if (close(fd_to) == -1)
