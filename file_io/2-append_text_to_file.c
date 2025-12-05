3bc813914cf6d59d46801eaef7de565bbc166609
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-05 12:32:18 +0000

    -> Ligne 38 : espace efface entre close et (fd)

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -35,7 +35,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		bytes_written = write(fd, text_content, len);
 		if (bytes_written == -1)
 		{
-			close (fd);
+			close(fd);
 			return (-1);
 		}
 	}
