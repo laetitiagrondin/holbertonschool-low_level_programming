4b48144782ce728719c2c7f3878ca93dccd787e7
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-05 09:53:43 +0000

    -> Deplacement de : - new_node = malloc ...; new_node->n = n; -> Ajout de : - if (idx == 0 ...) ... return (new_node); -> Changement dans le for () -> Suppression de : - condition if (current == NULL) - son return (NULL); -> Ce bloc, au-dessus, remplace par : - if (i < idx - 1) ... return (NULL);

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -19,21 +19,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	current = *h;
-	for (i = 0; i < idx; i++)
-	{
-		current = current->next;
-	}
-	if (current == NULL)
-	{
-		return (NULL);
-	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 	new_node->n = n;
+	if (idx == 0 || *h == NULL)
+	{
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h != NULL)
+		{
+			(*h)->prev = new_node;
+		}
+		*h = new_node;
+		return (new_node);
+	}
+	current = *h;
+	for (i = 0; (i < idx - 1) && (current->next != NULL); i++)
+	{
+		current = current->next;
+	}
+	if (i < idx - 1)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->next = current->next;
 	new_node->prev = current;
 	if (current->next != NULL)
