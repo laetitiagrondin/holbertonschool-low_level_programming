940164d211f3a5977405d02e1dbeda0445212a08
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-12-05 16:59:07 +0000

    -> Ajout de bibliotheques : - stdio.h : dprintf - stdlib.h : exit() - unistd.h : write(), close() - fcntl.h : open() -> Implementation de la fonction copy_file

---
+++ file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ * copy_file - Copies content of one file to another
+ * @file_from: The source file
+ * @file_to: The destination file
+ *
+ * Return: 0 on success, or a specific exit code on error
+ */
+int copy_file(const char *file_from, const char *file_to)
+{
+	int fd_from;
+	int fd_to;
+	int bytes_read;
+	int bytes_written;
+	char buffer[1024];
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+	return (0);
+}
